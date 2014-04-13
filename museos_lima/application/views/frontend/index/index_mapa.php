1f9b1e4ab1254748e23ea013509bedb9fda87844
Xavier <exrodriguezc@gmail.com>
2014-04-13 10:53:21 -0500

    cambio

--- museos_lima/application/views/frontend/index/index_mapa.php
+++
@@ -1,1 +0,0 @@
-<h1>Hola este es el Frontend</h1>
--- museos_lima/public/backend/js/eventos.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-function eliminar(url)
-{
-	if(confirm("Realmente desea Anular este registro?"))
-	{
-		window.location=url;
-	}
-}
